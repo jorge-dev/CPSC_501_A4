4b08210672ce9c82ad4265d9fa698065c36dbe6f
jorge.avila <jorge.avila@ucalgary.ca>
2021-12-12 03:31:42 -0700

    MNIST Third run --Achieved 95% performace by changing 2 hyperparameters

--- MNIST_starter.py
+++ MNIST_starter.py
@@ -143,13 +143,13 @@ def print_msg_box(msg, indent=1, width=None, title=None):
 
 
 input_neurons = 784
-hidden_neurons = [10]
+hidden_neurons = [50]
 output_neurons = 10
 sizes = [input_neurons] + hidden_neurons + [output_neurons]
 
 epochs = 10
 mini_batch_size = 10
-eta = 1
+eta = 2.5
 
 message = f"""Epochs: {epochs} 
 Mini-batch size: {mini_batch_size}
